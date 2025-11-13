c002aa28b4a396d961bc96b10649524a5e955ced
Andres Mtz <pandred25@gmail.com>
2025-11-13 09:20:46 -0600

    quite imagenes que no servian

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 prisma
   .$connect()
-  .then(() => console.log("Conectado a PostgreSQL con Prisma"))
+  .then(() => console.log("Conectado a PostgreSQL con Prisma En Neon"))
   .catch((err) => console.error("Error al conectar con Prisma:", err));
 
 export { prisma };
